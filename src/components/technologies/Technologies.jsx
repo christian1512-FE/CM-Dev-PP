04a79891c748dccac521f92c73eaf6656a1e1421
Your Name <christianmendez@Christians-MBP.attlocal.net>
2023-05-26 14:31:38 -0500

    techologies section done

--- src/components/technologies/Technologies.jsx
+++ src/components/technologies/Technologies.jsx
@@ -1,13 +1,46 @@
-import React from 'react'
+// import React from "react";
+import { FaReact, FaHtml5 } from "react-icons/fa";
+import { IoLogoJavascript, IoLogoCss3 } from "react-icons/io5";
+import { BsFillBootstrapFill } from "react-icons/bs";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper-bundle.css";
 import "./technologies.css";
 
-
 const Technologies = () => {
-  return (
-    <div>
-      Tech
-    </div>
-  )
-}
+  const swiperParams = {
+    spaceBetween: 10,
+    slidesPerView: 5,
+    direction: "horizontal",
+    loop: true,
+  };
 
-export default Technologies
+  return (
+    <div className="technologies background-color" id="about">
+      <Swiper {...swiperParams}>
+        <SwiperSlide>
+          <FaReact size={64} />
+          <p>React</p>
+        </SwiperSlide>
+        <SwiperSlide>
+          <IoLogoCss3 size={64} />
+          <p>CSS</p>
+        </SwiperSlide>
+        <SwiperSlide>
+          <FaHtml5 size={64} />
+          <p>HTML</p>
+        </SwiperSlide>
+        <SwiperSlide>
+          <IoLogoJavascript size={64} />
+          <p>JavaScript</p>
+        </SwiperSlide>
+        <SwiperSlide>
+          <BsFillBootstrapFill size={64} />
+          <p>Bootstrap</p>
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+};
+
+export default Technologies;

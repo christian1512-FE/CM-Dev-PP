41867a93aae652278410a631e60d8ec6bc1c2af2
Your Name <christianmendez@Christians-MBP.attlocal.net>
2023-06-07 10:23:10 -0500

    projects done

--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -1,11 +1,47 @@
 import React from "react";
+import weatherapp from "../../assets/weather-app.png";
+import { FaGithub } from "react-icons/fa";
+import { BiLinkExternal } from "react-icons/bi";
+
 import "./projects.css";
 
 const Projects = () => {
+  const livelink = "https://www.linkedin.com/in/christian-mendez-/"; 
+  const githublink = "https://christian1512-fe.github.io/DevCM-WA-3API/";
+
   return (
-    <div className="projects-container" id="projects">
-      <div className="project-card">
-        <p>Project cards</p>
+
+    <div className="portfolio-container" id="portfolio">
+      <div className="heading">
+        <h2>PORTFOLIO</h2>
+      </div>
+      <div className="projects">
+        <div className="img">
+          <img src={weatherapp} alt="working" />
+        </div>
+        <div className="content">
+          <h2>WEATHER API</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur porro ipsum commodi nemo
+            recusandae cupiditate minima ut, ullam praesentium ipsa eum et quia
+            voluptate nobis. Unde, magni?
+          </p>
+          <div className="links">
+            <button>
+            <a href={livelink}>
+              Code
+            <FaGithub />
+            </a>
+            </button>
+
+            <button>
+            <a href={githublink}>
+              Live Demo
+            <BiLinkExternal />
+            </a>
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );

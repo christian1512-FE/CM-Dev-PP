e4d018009bb66a8c12bc2a4200a5a97e021173ee
Your Name <christianmendez@Christians-MBP.attlocal.net>
2023-06-22 15:30:32 -0500

    added media to tech, fixed margin on nav

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,14 +5,18 @@ import cmlogo from "../../assets/web-logo.png";
 import "./navbar.css";
 
 const Navbar = () => {
+  const homelink = "https://christian1512-fe.github.io/CM-Dev-PP/#home";
   return (
     <div className="cm__navbar">
        <div className="cm__logo">
-             <img src={cmlogo} alt="weblogo" />    
+             <a href={homelink}>
+             <img src={cmlogo} alt="weblogo"/>
+             </a>
        </div>
         <div className="cm__navbar-links">
         <ul>
           <li><a href="#home">Home</a></li>
+          {/* <li><a href={homelink}>Home</a></li> */}
           <li><a href="#about">About</a></li>
           <li><a href="#portfolio">Portfolio</a></li>
           <li><a href="#contact">Contact</a></li>
